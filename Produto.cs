177626c9ac231c97267e581d5ffccb47d7d0fb7e
Ralf Prezia <62730168+ralfprezia@users.noreply.github.com>
2020-10-25 18:12:03 -0300

    Add files via upload

---
+++ Produto.cs
@@ -0,0 +1,50 @@
+﻿using System.Globalization;
+namespace Course {
+    class Produto {
+        //ATRIBUTOS PRIVADOS
+        private string _nome;
+        //PROPRIEDADES AUTO IMPLEMENTADAS
+        public double Preco { get; private set; }//auto propertiy = eu já defino que o preço não pode ser alterado
+        public int Quantidade { get; private set; }//auto propertie - somente leitura, não pode alterar a quantidade
+        //MÉTODO CONSTRUTOR
+        public Produto() {
+        }
+        //SOBRECARGA = OUTRO CONSTRUTOR
+        public Produto(string nome, double preco, int quantidade) {
+            _nome = nome;
+            Preco = preco;
+            Quantidade = quantidade;
+        }
+        //PROPRIEDADES CUSTOMIZADAS
+        //PROPERTY = como  o nome possui uma lógica cutomizada, ele não recebe a autopropertie
+        public string Nome {
+            //se o nome tiver menos que duas letras não haverá como alterálo, ele ira exibir o nome anterior
+            get { return _nome; }
+            set {
+                if (value != null && value.Length > 1) {
+                    _nome = value;
+                }
+            }
+        }
+       //OUTROS MÉTODOS
+        public double ValorTotalEmEstoque {//Calcula o valor total do estoque
+            get { return Preco * Quantidade; }
+        }
+        public void AdicionarProdutos(int quantidade) {// adiciona um produto  
+            Quantidade += quantidade;
+        }
+        public void RemoverProdutos(int quantidade) {// remove o produto
+            Quantidade -= quantidade;
+        }
+        public override string ToString() {//retorna a quantidade e nome separada por virgula e o valor com duas casas decimais e separados por virgula
+            return _nome
+            + ", $ "
+            + Preco.ToString("F2", CultureInfo.InvariantCulture)
+            + ", "
+            + Quantidade
+            + " unidades, Total: $ "
+            + ValorTotalEmEstoque.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization;
+
+namespace Course {
+    class Program {
+        static void Main(string[] args) {
+            
+            Produto p = new Produto("TV", 500.00, 10);//estanciando a classe = criano um objeto
+
+            p.Nome = "T";// este nome não será aceito por conta da definição do if em Nome na classe
+
+            Console.WriteLine(p.Nome);//imprime o Nome
+            Console.WriteLine(p.Preco);//imprime o valor do Preco
+            Console.WriteLine(p.Quantidade);//imprime a Quantidade
+
+        }
+    }
+}
